6ee2a71dd08094c647dc04e6eec2d65cf993b75e
Fernando <fernando.luiz92@hotmail.com>
2021-11-28 16:00:12 -0300

    lifo

---
+++ src/com/projeto/lifo/No.java
@@ -0,0 +1,31 @@
+package com.projeto.lifo;
+
+public class No {
+	
+	private int dado;
+	private No refNo=null;
+	
+	public No() {
+	}
+	public No(int dado) {
+		this.dado = dado ;
+	}
+	public int getDado() {
+		return dado;
+	}
+	public void setDado(int dado) {
+		this.dado = dado;
+	}
+	public No getRefNo() {
+		return refNo;
+	}
+	public void setRefNo(No reNo) {
+		this.refNo = reNo;
+	}
+	@Override
+	public String toString() {
+		return "No : {"+
+				dado+" }";
+	}
+
+}
---
+++ src/com/projeto/lifo/Pilha.java
@@ -0,0 +1,14 @@
+package com.projeto.lifo;
+
+public class Pilha {
+	
+	private No refNoEntradaPilha;
+
+	public Pilha() {
+	}
+	public Pilha(No refNoEntradaPilha) {
+		this.refNoEntradaPilha = refNoEntradaPilha;
+	}
+
+	
+}
